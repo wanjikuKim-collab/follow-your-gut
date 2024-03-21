654d056b4fc4ffb5a7b2d2c82e7ca90130e51040
Faith Kimani <wanjikukimani24@gmail.com>
2024-03-21 19:26:16 +0000

    Install react-hook-form

--- follow-your-gut/src/Components/AssessmentForm.jsx
+++ follow-your-gut/src/Components/AssessmentForm.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const AssessmentForm = () => {
   return (
-    <div>AssessmentForm</div>
+    // <div className='space-y-1 mb-2.5'>
+    // <label className='block'>Careers </label>
+    // <input className='block rounded border-0 bg-white px-3 w-full h-10 text-blue-dark' type="text" name='careers'  placeholder= {formTitle === 'Edit Course' ? '' : 'Careers' } value={formTitle === 'Edit Course' ? courses.careers: null}/>
+    // </div>
+    <div>
+      <form action="">
+        {/* <div className="form_input space-y-1 mb-2.5">
+          <label className='block' htmlFor=""></label>
+          <input className='block' type="text" name='age' placeholder='What is your age?' value={} />
+        </div> */}
+
+      </form>
+    </div>
   )
 }
 
