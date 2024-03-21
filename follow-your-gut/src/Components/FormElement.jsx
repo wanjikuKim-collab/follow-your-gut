f7bc3fd6d23765eeceb451e1c761d7228a33911d
Faith Kimani <wanjikukimani24@gmail.com>
2024-03-21 20:12:34 +0000

    Create a form element that captures the label and either the textarea or input field using props

---
+++ follow-your-gut/src/Components/FormElement.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FormElement = ({type, label, placeholder, fieldRef, hasError}) => {
+    const classes = "form-control w-full px-3 py-1.5 text-slate-700 rounded border border-solid border-slate-300 focus:border-lime-200 focus:outline-none"
+    
+    return (
+    <div className='form-group mb-6'>
+        <label className='block text-green-dark underline text-sm mb-2'>{label}</label>
+        {type === 'textarea' ? (
+            <textarea className={classes} rows= "5" placeholder= {placeholder} {...fieldRef}/>
+        ):(
+            <input className={classes} type= {type} placeholder= {placeholder} {...fieldRef}/>
+        )}
+        {hasError && <p className='form_error text-red-600 text-sm italic'>{`${label} is required`}</p>}
+    </div>
+  )
+}
+
+export default FormElement
