11f25f3cfdff641487d1271e6d3f607cf76477f6
Faith Kimani <wanjikukimani24@gmail.com>
2024-03-21 18:59:05 +0000

    Add and style assessment image

---
+++ follow-your-gut/src/Components/AssessmentForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AssessmentForm = () => {
+  return (
+    <div>AssessmentForm</div>
+  )
+}
+
+export default AssessmentForm
--- follow-your-gut/src/Pages/Assessment.js
+++ follow-your-gut/src/Pages/Assessment.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import assessment_img from '../Assets/assessment_form.png'
 
 const Assessment = () => {
   return (
-    <div>Assessment</div>
+    <div className='assessment'>
+        <div className='assessment_image_container flex flex-col text-left justify-center items-left px-6 pb-8 md:w-1/2 md:px-20'>
+            <h2 className='assessment_header text-2xl font-bold text-green-dark font-montserrat mb-4 md:text-5xl text-left'>Assessment Form</h2>
+            <img className='assessment_png ' src={assessment_img} alt='This shows a man dressing his slad'/>
+        </div>
+        <div className='assessment_form_container w-1/2'></div>
+        
+    </div>
   )
 }
 
