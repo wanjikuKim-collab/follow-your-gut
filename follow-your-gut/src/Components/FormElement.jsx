c165184cb20f25a9615981e95b43eff8d0434284
Faith Kimani <wanjikukimani24@gmail.com>
2024-03-22 11:50:31 +0000

    Change border visibility to bottom only, add mb-2 to the label field and remove the underline from the label

--- follow-your-gut/src/Components/FormElement.jsx
+++ follow-your-gut/src/Components/FormElement.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const FormElement = ({type, label, placeholder, fieldRef, hasError}) => {
-    const classes = "form-control w-full px-3 py-1.5 text-slate-700 rounded border border-solid border-slate-300 focus:border-lime-200 focus:outline-none"
+    const classes = "form-control w-full px-3 py-1.5 text-slate-700 rounded border-b-2 border-slate-300 focus:border-lime-200 focus:outline-none "
     
     return (
-    <div className='form-group mb-6'>
-        <label className='block text-left font-bold text-green-dark underline text-lg mb-2'>{label}</label>
+    <div className='form-group mb-4 mx-6'>
+        <label className='block text-left font-bold text-green-dark text-lg'>{label}</label>
         {type === 'textarea' ? (
             <textarea className={classes} rows= "5" placeholder= {placeholder} {...fieldRef}/>
         ):(
--- follow-your-gut/src/Pages/Assessment.js
+++ follow-your-gut/src/Pages/Assessment.js
@@ -4,8 +4,8 @@ import AssessmentForm from '../Components/AssessmentForm'
 
 const Assessment = () => {
   return (
-    <div className='assessment flex '>
-        <div className='assessment_image_container flex flex-col text-left justify-center items-left px-6 pb-8 md:w-1/2 md:px-20'>
+    <div className='assessment my-10 h-full flex '>
+        <div className='assessment_image_container flex flex-col text-left justify-between items-left px-6 pb-8 w-1/2 '>
             <h2 className='assessment_header text-2xl font-bold text-green-dark font-montserrat mb-4 md:text-5xl text-left'>Assessment Form</h2>
             <img className='assessment_png ' src={assessment_img} alt='This shows a man dressing his slad'/>
         </div>
