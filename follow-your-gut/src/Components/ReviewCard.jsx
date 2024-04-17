d42b40811ccc161ac55ae2b2d490a916d2aa87aa
Faith Kimani <wanjikukimani24@gmail.com>
2024-04-17 20:22:35 +0000

    Create the testimonial skeleton

---
+++ follow-your-gut/src/Components/ReviewCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ReviewCard() {
+  return (
+    <div className='testimonial_card'>
+        <i className='testimonail_card_icon'></i>
+        <p className="testimonial_card_content"></p>
+        <p className="avatar">
+            <img src="" alt="" />
+            <span></span>
+        </p>
+
+    </div>
+  )
+}
+
+export default ReviewCard
---
+++ follow-your-gut/src/Pages/Reviews.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Reviews() {
+  return (
+    <div className='testimonials'>
+        <h1 className='testimonial_header'>Our track record</h1>
+        <div className='testimonials_container'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Reviews
