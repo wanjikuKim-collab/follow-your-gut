84fda677d3fc5374bc56236fb8a805f75d024b20
Faith Kimani <wanjikukimani24@gmail.com>
2024-04-18 20:02:40 +0000

    Add hover effect

--- follow-your-gut/src/Components/ReviewCard.jsx
+++ follow-your-gut/src/Components/ReviewCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ReviewCard({testimonial, author, png}) {
   return (
-    <div className='testimonial_card rounded-xl p-8 bg-green-light mix-blend-luminosity'>
+    <div className='testimonial_card rounded-xl p-8 bg-green-light mix-blend-luminosity group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500'>
         <i className='testimonail_card_icon'></i>
         <p className="testimonial_card_content text-green-dark my-3 tracking-wide text-xl font-light opacity-50">{testimonial}</p>
         <div className="avatar">
--- follow-your-gut/src/Pages/Reviews.js
+++ follow-your-gut/src/Pages/Reviews.js
@@ -3,9 +3,9 @@ import ReviewCard from '../Components/ReviewCard'
 
 function Reviews() {
   return (
-    <div className='testimonials h-full text-center align-center p-14 items-center justify-center'>
+    <div className='testimonials h-full text-center align-center p-14 items-center justify-center cursor-pointer'>
       <h1 className='testimonial_header text-5xl p-20 font-bold text-green-dark font-montserrat mb-4 pb-4'>Our track record</h1>
-      <div className='testimonials_container flex flex-row m- mx-auto gap-8'>
+      <div className='testimonials_container flex flex-row m- mx-auto gap-8 group'>
         <ReviewCard testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!" author="Sarah M." png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" />
         <ReviewCard testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!" author="Sarah M." png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" />
         <ReviewCard testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!" author="Sarah M." png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" />
