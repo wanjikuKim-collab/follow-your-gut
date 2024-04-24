280b091cf2e9f308b8318ae45f1f2b09ada71388
Faith Kimani <wanjikukimani24@gmail.com>
2024-04-24 11:37:53 +0000

    Make the avatar div absolute to the parent div

--- follow-your-gut/src/Components/ReviewCard.jsx
+++ follow-your-gut/src/Components/ReviewCard.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from "react";
+import { MdStarRate } from "react-icons/md";
 
-function ReviewCard({testimonial, author, png}) {
+function ReviewCard({ testimonial, author, png }) {
   return (
-    <div className='testimonial_card rounded-xl p-8 bg-green-light mix-blend-luminosity group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500'>
-        <i className='testimonail_card_icon'></i>
-        <p className="testimonial_card_content text-green-dark my-3 tracking-wide text-xl font-light opacity-50">{testimonial}</p>
-        <div className="avatar">
-          <img class="w-20 h-20 mx-auto rounded-full" src={png} alt="user avatar" height="220" width="220" loading="lazy"/>
-          <span className='text-xl text-green-dark font-montserrat font-bold'>{author}</span>
-        </div>
-
+    <div className="testimonial_card relative w-96 rounded-xl px-8 pt-2 pb-20 bg-green-light mix-blend-luminosity group-hover:blur-sm hover:!blur-none group-hover:scale-[0.85] hover:!scale-100 duration-500">
+      <i className="testimonail_card_icon flex text-green-dark text-3xl p-1">
+        <MdStarRate /><MdStarRate /><MdStarRate /><MdStarRate /><MdStarRate />
+      </i>
+      <p className="testimonial_card_content font-montserrat text-green-dark my-3 text-left text-lg">
+        {testimonial}
+      </p>
+      <div className="avatar absolute bottom-1 text-left mx-auto flex items-center">
+        <img
+          class="w-20 h-20 rounded-full"
+          src={png}
+          alt="user avatar"
+          height="220"
+          width="220"
+          loading="lazy"
+        />
+        <span className="text-xl text-green-dark px-2 font-montserrat font-bold">
+          {author}
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ReviewCard
+export default ReviewCard;
--- follow-your-gut/src/Pages/Reviews.js
+++ follow-your-gut/src/Pages/Reviews.js
@@ -6,9 +6,21 @@ function Reviews() {
     <div className='testimonials h-full text-center align-center p-14 items-center justify-center cursor-pointer'>
       <h1 className='testimonial_header text-5xl p-20 font-bold text-green-dark font-montserrat mb-4 pb-4'>Our track record</h1>
       <div className='testimonials_container flex flex-row m- mx-auto gap-8 group'>
-        <ReviewCard testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!" author="Sarah M." png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" />
-        <ReviewCard testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!" author="Sarah M." png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" />
-        <ReviewCard testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!" author="Sarah M." png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" />
+        <ReviewCard 
+          testimonial="FYG has been a game-changer for my health journey! I used to feel lost in a sea of generic advice, but FYG's AI tailors everything to my needs. The delicious Kenyan recipes with local ingredients are a bonus – I'm eating healthier and feeling more energized than ever!"
+          author="Sarah M." 
+          png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/second_user.webp" 
+          />
+        <ReviewCard 
+          testimonial="As a busy professional, I don't have time to waste on figuring out what's best for my health. FYG takes the guesswork out of it. The AI-powered assessment gave me valuable insights, and the personalized meal plans make healthy eating effortless. I highly recommend FYG to anyone who wants to take control of their wellness." 
+          author=" David K." 
+          png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/third_user.webp"
+          />
+        <ReviewCard 
+          testimonial="I've tried countless diet and fitness apps in the past, but none of them stuck. FYG is different – it feels like a personalized coach in my pocket! The app understands my preferences and keeps me motivated with its recommendations. Plus, the FYG grocery lists make shopping for healthy ingredients a breeze." 
+          author="Aisha W." 
+          png="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/first_user.webp" 
+          />
       </div>
     </div>
   )
