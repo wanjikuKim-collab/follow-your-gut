ecdc4e57575a809c36bc3215a644ba099ded300e
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-04-30 22:23:43 +0300

    Mobile view for about us section

--- follow-your-gut/src/Pages/About.jsx
+++ follow-your-gut/src/Pages/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import mealplan from '../Assets/meal_plan.png'
 function About() {
   return (
-    <div className="flex flex-row h-full align-center p-14 items-center justify-center bg-green-light">
-      <div className="w-1/2 flex flex-col text-left justify-between items-left px-6">
+    <div className="flex md:flex-row flex-col h-full align-center p-6 items-center justify-center bg-green-light">
+      <div className="md:w-1/2 flex flex-col text-left justify-between items-left px-6">
         <h2 className="text-5xl font-bold text-green-dark font-montserrat mb-4 text-left pb-4">The FYG Approach to Wellness</h2>
         <h3 className="text-3xl font-montserrat font-bold text-green-dark mb-4">Powered by AI for a Personalized Journey</h3>
         <p className="text-green-dark font-light text-xl leading-normal text-left px-2">
@@ -17,7 +17,7 @@ function About() {
           recommendations to your specific needs and preferences.
         </p>
       </div>
-      <div className="w-1/2 flex items-center">
+      <div className="md:w-1/2 flex">
       <img
         className="m-2 h-[700] w-[600] object-cover rounded-2xl flex justify-center"
         src={mealplan}
