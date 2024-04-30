f3c2dfced4bcd7ca49f2121d252509ea94aa38f6
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-04-30 22:23:28 +0300

    Mobile view for feature cards and grid display instead of flex

--- follow-your-gut/src/Pages/Features.js
+++ follow-your-gut/src/Pages/Features.js
@@ -4,10 +4,10 @@ import { FaCheck } from "react-icons/fa";
 
 function Features() {
   return (
-    <div className="features h-full align-center p-14 items-center justify-center">
-      <h1 className="features_heading text-5xl font-bold text-green-dark font-montserrat mb-4 text-center pb-4">What We Provide</h1>
-      <p className="features_description text-slate-600 mb-6 tracking-wide text-2xl">Eat well, feel great. FYG uses AI to create personalized wellness plans just for you based on your assessment. </p>
-      <div className="feature_cards_container flex flex-row gap-4 justify-between">
+    <div className="features h-full align-center p-6 items-center justify-center">
+      <h1 className="features_heading text-3xl md:text-5xl font-bold text-green-dark font-montserrat mb-4 text-center pb-4">What We Provide</h1>
+      <p className="features_description text-slate-600 mb-6 tracking-wide text-xl md:text-2xl">Eat well, feel great. FYG uses AI to create personalized wellness plans just for you based on your assessment. </p>
+      <div className="feature_cards_container md:grid grid-cols-3 wrap gap-4 justify-between">
         <FeatureCard
           icon={<FaCheck />}
           heading="Assessment and Consultation"
