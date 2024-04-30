ee0a934a8b04fec710c4ca52c0e797c26433ad8c
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-04-30 22:22:48 +0300

    Mobile view for nutritionist section

--- follow-your-gut/src/Components/GetStarted.js
+++ follow-your-gut/src/Components/GetStarted.js
@@ -5,20 +5,20 @@ import CallToAction from "./CallToAction"
 function GetStarted() {
   return (
     <div
-      className={`flex flex-row h-full bg-green-light align-center p-14 items-center justify-center`}
+      className={`flex flex-col md:flex-row h-full bg-green-light align-center md:p-14 items-center justify-center md:text-left`}
     >
-      <div className="w-1/2 text-left justify-between items-left px-6">
-        <h2 className="text-5xl font-bold text-green-dark font-montserrat mb-4 text-left pb-4">
+      <div className="md:w-1/2 justify-between">
+        <h2 className=" text-3xl md:text-5xl font-bold text-green-dark font-montserrat mb-4 pb-4">
           Get started with one of our licensed nutritionists
         </h2>
         <img
-          className="border rounded-full"
+          className=" pb-2 rounded-full"
           src={nutritionist}
           alt="There is a nutritiomnist in a white lab coat displaying charts and graphs on a tablet"
         />
       </div>
-      <div className="w-1/2 flex flex-col items-center">
-        <p className="text-green-dark font-light text-2xl leading-normal text-left px-2">
+      <div className="md:w-1/2 flex flex-col items-center">
+        <p className="text-green-dark font-light text-xl md:text-2xl md:pl-14 leading-normal px-2">
           FYG empowers Kenyans with personalized meal plans using local
           ingredients. We believe gut health starts with a diverse diet, and aim
           to unlock its potential for a healthier young local ingredients. We
