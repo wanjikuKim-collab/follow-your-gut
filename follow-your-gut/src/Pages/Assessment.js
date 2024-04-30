b2dac4f9e44d5d8ed203c06e5a2ddea1b1c96754
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-04-30 22:22:09 +0300

    Mobile view for assessment form

--- follow-your-gut/src/Pages/Assessment.js
+++ follow-your-gut/src/Pages/Assessment.js
@@ -4,12 +4,12 @@ import AssessmentForm from '../Components/AssessmentForm'
 
 const Assessment = ({onSubmit}) => {
   return (
-    <div className='assessment my-10 h-full flex' id='assessment'>
-        <div className='assessment_image_container flex flex-col text-left justify-between items-left px-6 pb-8 w-1/2 '>
-            <h2 className='assessment_header text-2xl font-bold text-green-dark font-montserrat mb-4 md:text-5xl text-left'>Assessment Form</h2>
-            <img className='assessment_png ' src={assessment_img} alt='This shows a man dressing his slad'/>
+    <div className='assessment my-10 h-full flex flex-col md:flex-row' id='assessment'>
+        <div className='assessment_image_container flex flex-col md:text-left justify-between px-6 pb-8 md:w-1/2 '>
+            <h2 className='assessment_header text-3xl font-bold text-green-dark font-montserrat mb-4 md:text-5xl'>Assessment Form</h2>
+            <img className='assessment_png block max-w-full rounded-lg' src={assessment_img} alt='This shows a man dressing his slad'/>
         </div>
-        <div className='assessment_form_container w-1/2'>
+        <div className='assessment_form_container md:w-1/2'>
           <AssessmentForm onSubmit={onSubmit}/>
         </div>
         
